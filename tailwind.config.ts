4f8cef9a1c1e3d7a65d508492d2d1ec0fc8ac06c
Alexis Germain <alex@mbp-de-alexis.home>
2024-01-03 15:40:29 +0100

    0.0.1

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        primary: "#193D51",
+        secondary: "#FFF1CE",
+        tertiary: "#003566",
+      },
+      extend: {},
+    },
+  };
